d796e1d02a570ce1c897da4865d11b006a203b9c
plasorak <pierre.lasorak@gmail.com>
2018-08-03 09:34:57 +0100

    Minor modification to make this work. This is going to be anyway modified to fit in the EnergyFitterCore framework

---
+++ EnergyFitter_sig.py
@@ -0,0 +1,36 @@
+from EnergyFitterCore import EnergyFitterCore
+
+
+
+class EnergyFitterSigmoid(EnergyFitterCore):
+
+    def __init__(self):
+        super().__init__()
+        self.InputFile = "Config5.txt"
+        self.Debug = True
+        self.RandomSeed = 2055382905
+        self.BatchSize = 1
+        self.ActivationFunction = "relu"
+        self.nNeuron = 100
+        self.Minimiser = "adam"
+        self.LearningRate = 0.001
+        self.nIteration = 20000
+        self.TrainingFraction = 0.5
+        self.CrossValidationFraction = 0.25    
+
+
+def main():
+    EFS = EnergyFitterSigmoid()
+    EFS.InitialiseInputData()
+    EFS.Train()
+    EFS.ExportXML("file.xml")
+
+    
+if __name__ == "__main__":
+    main()
+
+
+    
+
+
+
